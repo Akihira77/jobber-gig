63df567b4f17f52e466a674877ba9f0d10ac6863
Akihira77 <tik.dikawahyu123@gmail.com>
2024-02-13 15:08:47 +0700

    fix environment configuration

--- src/@types/environment.d.ts
+++ src/@types/environment.d.ts
@@ -3,17 +3,14 @@ export {};
 declare global {
     namespace NodeJS {
         interface ProcessEnv {
-
             PORT: string;
             ENABLE_APM: string;
             DATABASE_URL: string;
             GATEWAY_JWT_TOKEN: string;
             JWT_TOKEN: string;
             NODE_ENV: string;
-            CLIENT_URL: string;
             API_GATEWAY_URL: string;
             RABBITMQ_ENDPOINT: string;
-            MYSQL_DB: string;
             CLOUD_NAME: string;
             CLOUD_API_KEY: string;
             CLOUD_API_SECRET: string;
